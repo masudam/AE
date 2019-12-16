8cd7b8d69cb817ba79750e8b9ee24c7d9bd94da0
masudam <masa.easygoing@gmail.com>
2019-12-16 14:10:35 +0900

    add model description

--- check_model.py
+++ check_model.py
@@ -9,6 +9,86 @@ import time
 import sys
 
 
+# Training Parameters
+learning_rate = 0.002
+num_steps = 1000000
+batch_size = 100
+
+display_step = 1000
+
+# Network Parameters
+num_hidden_1 = 1024 # 1st layer num features
+num_hidden_2 = 512 # 2nd layer num features (the latent dim)
+num_hidden_3 = 128
+num_input = 28800 # MNIST data input (img shape: 180*160)
+
+# tf Graph input (only pictures)
+X = tf.placeholder("float", [None, num_input])
+
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([num_input, num_hidden_1])),
+    'encoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2])),
+    'encoder_h3': tf.Variable(tf.random_normal([num_hidden_2, num_hidden_3])),
+    'decoder_h1': tf.Variable(tf.random_normal([num_hidden_3, num_hidden_2])),
+    'decoder_h2': tf.Variable(tf.random_normal([num_hidden_2, num_hidden_1])),
+    'decoder_h3': tf.Variable(tf.random_normal([num_hidden_1, num_input])),
+}
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([num_hidden_1])),
+    'encoder_b2': tf.Variable(tf.random_normal([num_hidden_2])),
+    'encoder_b3': tf.Variable(tf.random_normal([num_hidden_3])),
+    'decoder_b1': tf.Variable(tf.random_normal([num_hidden_2])),
+    'decoder_b2': tf.Variable(tf.random_normal([num_hidden_1])),
+    'decoder_b3': tf.Variable(tf.random_normal([num_input])),
+}
+
+
+
+# Building the encoder
+def encoder(x):
+    # Encoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
+                                   biases['encoder_b1']))
+    # Encoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
+                                   biases['encoder_b2']))
+    # Encoder Hidden layer with sigmoid activation #3
+    layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['encoder_h3']),
+                                   biases['encoder_b3']))
+    return layer_3
+
+
+# Building the decoder
+def decoder(x):
+    # Decoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
+                                   biases['decoder_b1']))
+    # Decoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
+                                   biases['decoder_b2']))
+    # Decoder Hidden layer with sigmoid activation #3
+    layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['decoder_h3']),
+                                   biases['decoder_b3']))
+    return layer_3
+
+# Construct model
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+
+# Prediction
+y_pred = decoder_op
+# Targets (Labels) are the input data.
+y_true = X
+
+# Define loss and optimizer, minimize the squared error
+loss = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+
+
 # 画像をとってくる
 def make_img(sess):
     dir_name="../img_data"
@@ -35,7 +115,6 @@ if __name__ == "__main__":
     inp = sys.argv
     model_dir = "../models/" + inp[1] + '/my-model.ckpt'
     with tf.Session() as sess:
-        init = tf.global_variables_initializer()
         sess.run(init)
         #まずはmodelを読み込む
         print("restore model...")
